241a7ea278c68756036ff328bd3a9f876e154a78
xxc <1660669610@qq.com>
2019-12-23 14:06:25 +0800

    3

---
+++ src/main/java/com/xxc/demo1/git1/GitTest.java
@@ -0,0 +1,19 @@
+package com.xxc.demo1.git1;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitTest {
+    public void  getGit(){
+        System.out.println("1111" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "");
+    }
+
+}
